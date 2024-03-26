2d54f855112be018f45ed9d42a8a22b68d6d6468
nikhilchalis <nikhilchalis@gmail.com>
2024-03-26 16:38:24 +1100

    initialised display

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 GREY = (128, 128, 128)
 
+HEIGHT = 800
+WIDTH = 1200
+FPS = 25
+
+
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    pg.display.set_caption("Pathfinding algorithm visualisations")
+    clock = pg.time.Clock()
+
+    screen.fill(BLACK)
+
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        clock.tick(FPS)
+        pg.display.flip()
+        screen.fill(BLACK)
+
+if __name__=="__main__":
+    main()
